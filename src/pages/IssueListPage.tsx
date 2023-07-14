e038bfa4eae0a99adcc3b5386ed178bbda0044c3
Suhwan0818 <tnghks1997@gmail.com>
2023-07-14 23:27:24 +0900

    🚑 build 과정에서 eslint 문제 발생 해결

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef } from 'react';
 import { IssueWrap } from '../components/IssueList/IssueListPresenter';
 import IssueListItem from '../components/IssueList/IssueListItem';
